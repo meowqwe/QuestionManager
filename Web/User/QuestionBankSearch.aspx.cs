329ca7a7b60b8e766c2c254c31213582034076c1
meowqwe <47526207+meowqwe@users.noreply.github.com>
2020-06-09 17:16:13 +0800

    temp1

--- Web/User/QuestionBankSearch.aspx.cs
+++ Web/User/QuestionBankSearch.aspx.cs
@@ -1,6 +1,7 @@
 ﻿using QBModels;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -11,6 +12,12 @@ public partial class User_QuestionBankSearch : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
         User user = (User)Session["CurUser"];
+        string sql = "SELECT * FROM @questionbank";
+        SqlParameter[] para = new SqlParameter[]
+        {
+            new SqlParameter("@questionbank","questionbank")
+        };
+        
     }
 
     protected void btnSearch_Click(object sender, EventArgs e)
