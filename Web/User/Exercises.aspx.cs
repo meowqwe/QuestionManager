2a4cb68465060df7fb9c6bd9e2a1f0e28011a02b
meowqwe <47526207+meowqwe@users.noreply.github.com>
2020-06-10 00:54:42 +0800

    sleep

--- Web/User/Exercises.aspx.cs
+++ Web/User/Exercises.aspx.cs
@@ -11,13 +11,15 @@ public partial class User_Exercises : System.Web.UI.Page
 {
     private QuestionBank questionbank;
     private IList<title> tlist;
+    private int titlenum;
 
     public QuestionBank Questionbank { get => questionbank= (QuestionBank)Session["CurQuestionBank"]; set => questionbank = value; }
     public IList<title> Tlist { get => tlist=QuestionBankManager.GetAllTitle(Questionbank); set => tlist = value; }
+    public int Titlenum { get => titlenum; set => titlenum = value; }
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        Titlenum = 0;
     }
 
     protected void NEXT_Click(object sender, EventArgs e)
@@ -28,5 +30,15 @@ public partial class User_Exercises : System.Web.UI.Page
     protected void Submit_Click(object sender, EventArgs e)
     {
 
+    }
+    protected void LoadTitle(int Titlenum)
+    {
+        lblType.Text = Tlist[Titlenum].Ttype1;
+        lblSubject.Text = Tlist[Titlenum].Tsubject1;
+        lblStem.Text = Tlist[Titlenum].Tstem1;
+        if (Tlist[Titlenum].Ttype1.Equals("选择"))
+        {
+            options options = new options();
+        }
     }
 }
